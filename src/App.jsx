2fde61fd28960e27091d6a297da8fa4b543ff111
Eternal128 <jamwilhan@gmail.com>
2025-10-07 02:10:52 -0400

    Creating my first React app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter } from "react-router-dom";
+
+import { About, Contact, Experience, Feedbacks, Hero, Loader, Navbar, Tech, Works, StarsCanvas } from './components';
+
+const App = () => {
+  return (
+    <div>
+      3D portoflio
+    </div>
+  );
+}
+
+export default App;
