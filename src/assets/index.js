5a9dfacf1c26dbdc862d6929f7db851ce3b70c00
Eternal128 <jamwilhan@gmail.com>
2025-10-07 06:56:52 -0400

    feat/added experience

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,10 +21,9 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import ey from "./company/ey.png"
+import metrodata from "./company/metrodata.png";
+import japfa from "./company/japfa.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -52,11 +51,10 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
   carrent,
   jobit,
   tripguide,
+  ey,
+  metrodata,
+  japfa,
 };
