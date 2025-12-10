eaa82184ea64488f572c158b524d5eceb75640c5
Eternal128 <jamwilhan@gmail.com>
2025-12-09 19:51:20 -0500

    changed model

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -5,10 +5,10 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Earth = () => {
-  const earth = useGLTF("./planet/scene.gltf");
+  const earth = useGLTF("./pc/scene.gltf");
 
   return (
-    <primitive object={earth.scene} scale={1} position-y={0} rotation-y={0} />
+    <primitive object={earth.scene} scale={1} position-x={0.5}  position-y={-1} rotation-y={0} />
   );
 };
 
@@ -22,8 +22,8 @@ const EarthCanvas = () => {
       camera={{
         fov: 45,
         near: 0.1,
-        far: 200,
-        position: [0, 3, 6],
+        far: 300,
+        position: [0, 6, 6],
       }}
     >
       <Suspense fallback={<CanvasLoader />}>
